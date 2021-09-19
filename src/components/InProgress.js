674e3fc69864d8ea60dc987a8abbb7185b90cf5e
Cris Acosta <iamcris87@gmail.com>
2021-09-19 22:26:00 +0400

    reset secs to 1

--- src/components/InProgress.js
+++ src/components/InProgress.js
@@ -26,14 +26,14 @@ const InProgress = ({ item, jobCompleted }) => {
         const timeCounter = setInterval(() => {
         setSecs( secs => secs + 1)
 
-        const hours = Math.floor(secs / (60 * 60));
-
+        
         const divisor_for_minutes = secs % (60 * 60);
         const minutes = Math.floor(divisor_for_minutes / 60);
-
+        
         const divisor_for_seconds = divisor_for_minutes % 60;
         const seconds = Math.ceil(divisor_for_seconds);
         
+        const hours = Math.floor(secs / (60 * 60));
         
         setTimer({
           hour: hours,
